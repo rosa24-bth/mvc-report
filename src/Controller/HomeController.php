30981259d944226549df9bcc237c1510d8cefe65
rosa24 <rosa24@student.bth.se>
2025-04-15 10:45:48 +0200

    Add pig game

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyControllerTwig extends AbstractController
+class HomeController extends AbstractController
 {
     /*
     #[Route("/lucky/number/twig", name: "lucky_number")]
---
+++ src/Dice/Dice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Dice;
+
+class Dice
+{
+    protected int $value;
+
+    public function roll(): int
+    {
+        $this->value = random_int(1, 6);
+        return $this->value;
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function getAsString(): string
+    {
+        return "[{$this->value}]";
+    }
+}
