de15a0bc66308fa1c44172a97d6afcff2a60815a
rosa24 <rosa24@student.bth.se>
2025-04-15 15:57:32 +0200

    Update deck template to show cards as strings

---
+++ src/Card/Card.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Card;
+
+class Card
+{
+    private string $value;
+    private string $suit;
+
+    public function __construct(string $value, string $suit)
+    {
+        $this->value = $value;
+        $this->suit = $suit;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function getSuit(): string
+    {
+        return $this->suit;
+    }
+
+    public function getAsString(): string
+    {
+        return "[{$this->value}{$this->suit}]";
+    }
+}
