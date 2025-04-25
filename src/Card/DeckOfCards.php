7d74e9c2f13a7929b8f77caa7e4fc5c055cb4378
rosa24 <rosa24@student.bth.se>
2025-04-25 10:55:14 +0200

    Update card suits with color

--- src/Card/DeckOfCards.php
+++ src/Card/DeckOfCards.php
@@ -8,7 +8,7 @@ class DeckOfCards
 
     public function __construct()
     {
-        $suits = ['♠', '♥', '♦', '♣'];
+        $suits = ['♠️', '♥️', '♦️', '♣️'];
         $values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
         foreach ($suits as $suit) {
